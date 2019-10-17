93e544650566d747f1872ecec69451b015a41053
Manubhav <54354723+Manubhav@users.noreply.github.com>
2019-10-17 23:17:02 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+struct node		//THIS STRUCTURE DECLARATION DEFINES A NEW DATATYPE CALLED NODE THAT REPRESENTS A LINKED LOST NODE
+{
+int info;
+struct node*next;	
+};
+typedef struct node NODE;
